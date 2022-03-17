3bb7959d49d61cb8ffdb29aeefabca3175a6b70f
pabloipl <101835474+pabloipl@users.noreply.github.com>
2022-03-17 20:54:26 -0300

    Add files via upload

---
+++ Operacion.java
@@ -0,0 +1,15 @@
+
+public class Operacion {
+    public static void main(String[] args) {
+        System.out.print("La suma es: " + Suma(2, 3) + "\n");
+        System.out.print("La resta es: " + Resta(2, 3));
+    }
+
+    public static int Suma(int x, int y) {
+        return x + y;
+    }
+
+    public static int Resta(int x, int y){
+        return x-y;
+    }
+}
